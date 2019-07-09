86d26caa25125fe5ee4c63c3f75a1bdc2ee373e2
SuperLandy <44797172+SuperLandy@users.noreply.github.com>
2019-07-09 11:16:50 +0800

    nginx处理静态文件

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 cd /opt/cmdb/
 python manage.py makemigrations
 python manage.py migrate
-nohup python manage.py runserver 0.0.0.0:80 --insecure 1>/dev/null 2>/dev/null &
+nohup python manage.py runserver 0.0.0.0:8080 1>/dev/null 2>/dev/null &
